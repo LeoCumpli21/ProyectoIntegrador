6733f4bd5080b97e9f1c87e5c326c5bcf5c4c816
Leonardo Cumplido <cumplileo@gmail.com>
2021-11-29 12:53:08 -0600

    nuevo atributos y métodos en la clase Transacción

--- Transaccion.cpp
+++ Transaccion.cpp
@@ -3,15 +3,25 @@
 
 Transaccion::Transaccion()
 {
+    tipo = "";
     id = 0;
     estado = false;
-    tipo = "";
+    fecha = Fecha();
+    producto = Producto();
 }
 
-Transaccion::Transaccion(int _id)
+Transaccion::Transaccion(std::string _tipo, int _id, bool _estado, Fecha _fecha, Producto _producto)
 {
+    tipo = _tipo;
     id = _id;
-    estado = false;
+    estado = _estado;
+    fecha = _fecha;
+    producto = _producto;
+}
+
+std::string Transaccion::getTipo() const
+{
+    return tipo;
 }
 
 int Transaccion::getId() const
@@ -22,6 +32,11 @@ int Transaccion::getId() const
 bool Transaccion::getEstado() const
 {
     return estado;
+}
+
+void Transaccion::setTipo(std::string _tipo)
+{
+    tipo = _tipo;
 }
 
 void Transaccion::setId(int _id)
@@ -37,4 +52,15 @@ void Transaccion::setEstado(bool _estado)
 void Transaccion::completarTransaccion()
 {
     estado = true;
+}
+
+void Transaccion::imprimirTransaccion()
+{
+    std::cout << "Tipo: " << tipo << std::endl;
+    std::cout << "Id: " << id << std::endl;
+    std::cout << "Estado: " << estado << std::endl;
+    std::cout << "Fecha: ";
+    fecha.mostrarFecha();
+    std::cout << std::endl;
+    producto.imprimirProducto();
 }
